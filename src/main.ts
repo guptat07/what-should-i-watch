30a43caed3542b5d2a3ef089d89962aed4c32706
Aryan "Tony" Gupta <guptat07@gmail.com>
2026-07-22 15:07:31 -0400

    Trying to learn api and vercel functions

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,42 @@
+import callGemini from '../api/callgemini.ts';
+
 const form = document.getElementById('form') as HTMLFormElement;
 const outputDiv = document.getElementById('output') as HTMLDivElement;
 
+// const geminiKey: string = import.meta.env.GEMINI_API_KEY;
 
-const showResult = (event: SubmitEvent) =>
+const showResult = async (event: SubmitEvent) =>
 {
+  event.preventDefault();
   // `as` is a type assertion, not renaming (unlike how it works in python imports)
   // this doesn't actually guarantee freedom from runtime errors!!!
   // always ensure this is actually an input element in the HTML.
   const inputElement = document.getElementById('user-input') as HTMLInputElement;
-  if (inputElement != null)
+  if (!inputElement)
   {
-    let input: string = inputElement.value;
-    outputDiv.innerText = input;
+    return;
+  }
+  const input: string = inputElement.value;
+  if (!input)
+  {
+    return;
   }
 
-  event.preventDefault();
+  try
+  {
+    const request: Request = new Request('google.com');
+    const data: Response = await callGemini.fetch(request);
+    console.log(data);
+  }
+  catch (error)
+  {
+    console.error(error);
+  }
+  // if (inputElement != null)
+  // {
+  //   // let input: string = inputElement.value;
+  //   outputDiv.innerText = ;
+  // }
 }
 
 form.addEventListener('submit', showResult);
