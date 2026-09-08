a70edf07a9233a2b93af8ce43e71a1cdaa421319
Aryan "Tony" Gupta <guptat07@gmail.com>
2026-09-08 16:41:05 -0400

    More or less complete. Launchable?

--- src/main.ts
+++ src/main.ts
@@ -31,8 +31,53 @@ const showResult = async (event: SubmitEvent) =>
     });
 
     const data = await response.json();
-    console.log(data);
-    outputDiv.innerText = JSON.stringify(data, null, 2);
+
+    // Turn Data into styled results
+    // outputDiv will hold 10 movieDivs
+    // a movieDiv will hold a posterDiv and a movieInfoDiv (flex row)
+    // a movieInfoDiv will hold a title, year, and overview (flex col)
+    outputDiv.replaceChildren();
+    for (const movie of data) {
+      const movieDiv: HTMLDivElement = document.createElement("div");
+      movieDiv.classList.add("movie-container");
+
+      const posterDiv: HTMLDivElement = document.createElement("div");
+      posterDiv.classList.add("poster-container");
+
+      const poster: HTMLImageElement = document.createElement("img");
+      poster.classList.add("poster");
+      poster.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+      poster.alt = `Poster of ${movie.title}`;
+      poster.loading = 'lazy';
+      posterDiv.appendChild(poster);
+
+      movieDiv.appendChild(posterDiv);
+
+      const movieInfoDiv: HTMLDivElement = document.createElement("div");
+      movieInfoDiv.classList.add("movie-info-container");
+
+      const title: HTMLHeadingElement = document.createElement("h2");
+      title.classList.add("movie-info-header");
+      title.innerText = `${movie.title.toUpperCase()}`;
+
+      const year: HTMLHeadingElement = document.createElement("h2");
+      year.classList.add("movie-info-header");
+      year.innerText = `${movie.release_date.slice(0, 4)}`;
+
+      const overview: HTMLParagraphElement = document.createElement("p");
+      overview.classList.add("movie-info-overview");
+      overview.innerText = `${movie.overview}`;
+
+      movieInfoDiv.appendChild(title);
+      movieInfoDiv.appendChild(year);
+      movieInfoDiv.appendChild(overview);
+      movieDiv.appendChild(movieInfoDiv);
+
+      outputDiv.appendChild(movieDiv);
+    }
+
+    // console.log(data);
+    // outputDiv.innerText = JSON.stringify(data, null, 2);
   }
   catch (error)
   {
